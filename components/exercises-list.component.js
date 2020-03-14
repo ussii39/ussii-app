a34585c6da579f335475871dbf9ddad5d1a03b05
ussii39 <60258658+ussii39@users.noreply.github.com>
2020-03-14 16:38:12 +0900

    Add files via upload

---
+++ components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class exercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>you are on the Exercises LIst components! </p>
+            </div>
+        )
+    }
+}
